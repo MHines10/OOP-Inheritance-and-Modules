5a6b3d594b8457f2e639bbbecb4263a3a8632e2f
MHines10 <cd.mhines@gmail.com>
2022-12-13 19:57:07 -0500

    first commit

---
+++ exer_mods/__init__.py
@@ -0,0 +1,14 @@
+import math
+
+# Defines area of a circle
+
+def area_of_crcl(radius): 
+  pi = 3.14
+  area = pi * radius**2 
+  return area
+
+
+# Finds Hypotenuse of right angle
+
+def hptns(a,b):
+  return math.sqrt(a**2 + b**2)
